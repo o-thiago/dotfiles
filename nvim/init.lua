05a714ad2649f0c5e4ac0e8680a26add71cbe945
o-thiago <119901046+o-thiago@users.noreply.github.com>
2023-12-24 19:12:24 -0400

    Some basic and undone neovim configuration

---
+++ nvim/init.lua
@@ -0,0 +1,43 @@
+require("thiago")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+-- PLUGINS --
+local plugins = {
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme gruvbox]])
+
+			local alphahls = {"Normal", "NormalFloat"}
+			for i, hi in pairs(alphahls) do
+				vim.api.nvim_set_hl(0, hi, { bg = "none" })
+			end
+		end
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+		}
+	}
+}
+
+require("lazy").setup(plugins, opts)
---
+++ nvim/lua/thiago/set.lua
@@ -0,0 +1,16 @@
+-- NUMBER LINES --
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- TAB SIZES --
+local tablength = 4
+
+vim.opt.tabstop = tablength
+vim.opt.softtabstop = tablength
+vim.opt.shiftwidth = tablength
+
+vim.opt.smartindent = true
+
+-- 
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "80"
