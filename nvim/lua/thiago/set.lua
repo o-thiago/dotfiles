22e21dcb78bf3d962a60c57a14facb085a95ed0c
o-thiago <119901046+o-thiago@users.noreply.github.com>
2023-12-25 01:15:47 -0400

    Adds some settings to my nvim config

--- nvim/lua/thiago/set.lua
+++ nvim/lua/thiago/set.lua
@@ -11,6 +11,18 @@ vim.opt.shiftwidth = tablength
 
 vim.opt.smartindent = true
 
--- 
+-- RIGHT COLUMN --
 vim.opt.signcolumn = "yes"
 vim.opt.colorcolumn = "80"
+
+-- SEARCH --
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- UNDOS --
+vim.opt.swapfile = false
+-- i shoul probably setup a way to proper backup stuff... but i need to study
+-- this before!
+
+-- SCROLL --
+vim.opt.scrolloff = 4
